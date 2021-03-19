b65abc6ab6e51ad46c8af190bd43def3f473e1eb
Generally2 <dailyspecial_719@yahoo.com>
2021-03-18 23:23:16 -0600

    added gt_guacamole recipe to GTinaActivity and updated green_chili recipe and pb_squares pdf files

--- app/src/main/java/com/generally2/cooboo/GTinaActivity.java
+++ app/src/main/java/com/generally2/cooboo/GTinaActivity.java
@@ -19,7 +19,7 @@ public class GTinaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_g_tina);
 
         String[] gtTopics;
-        int[] gtImages = {R.drawable.enchiladas_menu,R.drawable.tamales_menu, R.drawable.potato_salad_menu,
+        int[] gtImages = {R.drawable.enchiladas_menu, R.drawable.guac_menu, R.drawable.tamales_menu, R.drawable.potato_salad_menu,
                 R.drawable.tuna_menu, R.drawable.pb_menu, R.drawable.zucchini_muffin_menu, R.drawable.mm_cookie_menu};
 
         Resources res = getResources();
@@ -41,26 +41,30 @@ public class GTinaActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
                 if (position == 1){
+                    Intent intent = new Intent(GTinaActivity.this, Guacamole.class);
+                    startActivity(intent);
+                }
+                if (position == 2){
                     Intent intent = new Intent(GTinaActivity.this, Tamales.class);
                     startActivity(intent);
                 }
-                if (position ==2){
+                if (position == 3){
                     Intent intent = new Intent(GTinaActivity.this, PotatoSalad.class);
                     startActivity(intent);
                 }
-                if (position == 3){
+                if (position == 4){
                     Intent intent = new Intent(GTinaActivity.this, TunaCasserole.class);
                     startActivity(intent);
                 }
-                if (position == 4){
+                if (position == 5){
                     Intent intent = new Intent(GTinaActivity.this, PBSquares.class);
                     startActivity(intent);
                 }
-                if (position == 5){
+                if (position == 6){
                     Intent intent = new Intent(GTinaActivity.this, ZucchiniMuffin.class);
                     startActivity(intent);
                 }
-                if (position == 6){
+                if (position == 7){
                     Intent intent = new Intent(GTinaActivity.this, ChipperChamps.class);
                     startActivity(intent);
                 }
